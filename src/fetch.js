1f5ac36da279ffe8ec40362ee931599931c56f3b
Alexey Zabrodsky <elmigranto@gmail.com>
2016-10-03 20:26:00 +0500

    Initial game fetching with relevant `./bin` script.

--- src/fetch.js
+++ src/fetch.js
@@ -6,40 +6,29 @@ const {getJson, arrays, tryBoth, firstResult} = require('./utils');
 
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = parseInt(process.env.PORT, 10) || 3000;
-
-// Helper to get stuff from API.
-//
-// f = getJson('coordinator', {withAuth: true})
-// f('alice', 'games/active')
-const fetch = (service, {collection, withAuth}) => (username, ...rest) => {
-  const base = `http://${HOST}:${PORT}/${service}/v1`;
-  const auth = withAuth
-    ? `/auth/${process.env.API_SECRET}.${username}/`
-    : `/${username}/`;
-
-  const itemId = rest.length === 2 ? `/${rest[0]}` : '';
-  const callback = rest[rest.length - 1]; // last argument is callback
-
-  getJson(base + auth + collection + itemId, callback);
-};
+const endpoint = (service, sub) => `http://${HOST}:${PORT}/${service}/v1${sub}`;
+const auth = (username) => `auth/${process.env.API_SECRET}.${username}`;
 
 const collections = {
   // list of games without any game data
-  // fn(username, callback)
-  coordinator: fetch('coordinator', {
-    collection: 'active-games',
-    withAuth: true
-  }),
+  coordinator: (username, callback) => getJson(
+    endpoint('coordinator', `/${auth(username)}/active-games`),
+    callback
+  ),
 
   // actual game info (from turngame)
-  // fn(username, gameId, callback)
-  activeGames: fetch('turngame', {
-    collection: 'games',
-    withAuth: true
-  })
+  activeGames: (username, gameId, callback) => getJson(
+    endpoint('turngame', `/${auth(username)}/games/${gameId}`),
+    callback
+  ),
+
+  archivedGames: (username, callback) => getJson(
+    endpoint('statistics', `/triominos/v1/${username}/archive`),
+    callback
+  )
 };
 
-const fetchActiveGame = (alice, bob, callback) => {
+const fetchCoordinatorGame = (status) => (alice, bob, callback) => {
   // TODO
   // probably query only single player…
   const participants = [alice, bob];
@@ -48,9 +37,15 @@ const fetchActiveGame = (alice, bob, callback) => {
     participants,
     collections.coordinator,
     (err, games) => {
+      if (err)
+        return callback(err);
+
       const uniqueGames = lodash.uniqBy(games, 'id');
       const gamesBetween = uniqueGames
-        .filter(game => arrays.equalWhenSorted(game.players, participants))
+        .filter(game => {
+          return (game.status === status)
+            && arrays.equalWhenSorted(game.players, participants);
+        })
         .sort((left, right) => right.endTime - left.endTime);
 
       if (gamesBetween.length) {
@@ -66,6 +61,27 @@ const fetchActiveGame = (alice, bob, callback) => {
   );
 };
 
+const fetchFinishedCoordinatorGame = fetchCoordinatorGame('gameover');
+const fetchInProgressCoordinatorGame = fetchCoordinatorGame('active');
+
+const fetchArchivedGame = (alice, bob, callback) => {
+  const participants = [alice, bob];
+
+  collections.archivedGames(alice, (err, stats) => {
+    if (err)
+      return callback(err);
+
+    const gamesBetween = lodash.map(stats, 'game')
+      .filter(game => arrays.equalWhenSorted(
+        lodash.map(game.players, 'username'),
+        participants
+      ))
+      .sort((left, right) => right.endTime - left.endTime);
+
+    callback(null, (gamesBetween.length > 0) ? gamesBetween[0] : null);
+  });
+};
+
 // Fetches game between 2 players.
 // callback(err, game|null)
 //
@@ -76,8 +92,21 @@ const fetchActiveGame = (alice, bob, callback) => {
 // 3. from coordinator (`active`) but in `progress`;
 // 4. null
 const fetchGame = (alice, bob, callback) => {
+  console.log('fetchGame()', alice, bob);
+
   firstResult(
-    (cb) => fetchActiveGame(alice, bob, cb),
+    (cb) => {
+      console.log('  trying finished coordinator game…');
+      fetchFinishedCoordinatorGame(alice, bob, cb);
+    },
+    (cb) => {
+      console.log('  trying archived games…');
+      fetchArchivedGame(alice, bob, cb);
+    },
+    (cb) => {
+      console.log('  trying in-progress coordinator game…');
+      fetchInProgressCoordinatorGame(alice, bob, cb);
+    },
     callback
   );
 };
--- test/analyzer/fetch.test.js
+++ test/analyzer/fetch.test.js
@@ -17,7 +17,7 @@ describe('Fetching stuff', () => {
   };
 
   describe('fetchGame()', () => {
-    it('retrieves active finishined games', (done) => {
+    it('retrieves finishined coordinator games', (done) => {
       fetchGame('p13', 'p07', (err, game) => {
         expect(err).to.be.null;
         expect(game).to.be.ok;
@@ -27,10 +27,39 @@ describe('Fetching stuff', () => {
     });
 
     it('retrieves archived games', (done) => {
-      fetchGame('alice', 'bob', (err, game) => {
+      fetchGame('p00', 'p04', (err, game) => {
         expect(err).to.be.null;
         expect(game).to.be.ok;
-        // expectJson(game, …)
+        expect(game).to.eql({
+          id: '989fd29d0b636ac09c7ec947c900f049',
+          date: 1439500003,
+          players: [{
+            username: 'p04',
+            score: -89
+          }, {
+            username: 'p00',
+            score: -100
+          }]
+        });
+        done();
+      });
+    });
+
+    it('fetches in progress coordinator games', (done) => {
+      fetchGame('p07', 'p05', (err, game) => {
+        expect(err).to.be.null;
+        expect(game).to.be.ok;
+        expectJson(game, './sample-data/turngame/v1/auth/secret.p05/games/f2af83ca45f60909680c38eac245ffe4');
+        done();
+      });
+    });
+
+    it('returns `null` if no games between players are found', (done) => {
+      // Samples are a bit fragile, so we are trying "solo" game,
+      // so coordinator and statistics have something to say.
+      fetchGame('p00', 'p00', (err, game) => {
+        expect(err).to.be.null;
+        expect(game).to.be.null;
         done();
       });
     });
