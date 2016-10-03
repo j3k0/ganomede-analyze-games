0b28330ec93d70865d8e3e3a6fc107c0272d381c
Alexey Zabrodsky <elmigranto@gmail.com>
2016-10-03 18:51:22 +0500

    Move out some stuff to utils.

--- src/fetch.js
+++ src/fetch.js
@@ -2,7 +2,7 @@
 
 const async = require('async');
 const lodash = require('lodash');
-const {getJson, arrays} = require('./utils');
+const {getJson, arrays, tryBoth, firstResult} = require('./utils');
 
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = parseInt(process.env.PORT, 10) || 3000;
@@ -80,70 +80,6 @@ const fetchGame = (alice, bob, callback) => {
     (cb) => fetchActiveGame(alice, bob, cb),
     callback
   );
-};
-
-// Tries calling functions one after the other.
-// Whichever returns result, "wins", meaning others won't be run.
-// If previous returns null, tries the other.
-// If previous fails, all fail.
-// If all return null, result is null.
-const firstResult = (...args) => {
-  const funcs = args.slice(0, -1);
-  const callback = args[args.length - 1];
-
-  let currentFunction = 0;
-  let currentResult = null;
-
-  const callFn = (cb) => {
-    const fn = funcs[currentFunction++];
-
-    fn((err, result) => {
-      if (err)
-        return cb(err);
-
-      currentResult = result;
-      cb();
-    });
-  };
-
-  async.whilst(
-    () => !currentResult && (currentFunction < funcs.length),
-    callFn,
-    (err) => callback(err, currentResult)
-  );
-};
-
-// Tries calling fnA and fnB.
-// If both succeed, checks that JSON documents are the same, cb(err) otherwise.
-// If one succeeds, returns that.
-// If none succeed, returns error of fnA.
-const tryBoth = (fnA, fnB, callback) => {
-  const try_ = (fn) => (cb) => {
-    fn((err, result) => cb(null, {
-      err: err || null,
-      result: err ? null : result
-    }));
-  };
-
-  const tries = [fnA, fnB].map(try_);
-
-  async.parallel(tries, (err, [retA, retB]) => {
-    if (err)
-      return callback(err);
-
-    if (retA.err && retB.err) {
-      callback(retA.err);
-    }
-    else if (retA.result && retB.result) {
-      const same = lodash.isEqual(retA.result, retB.result);
-      return same
-        ? callback(null, retA.result)
-        : callback(new Error('tryBoth() got conflicting result'));
-    }
-    else {
-      callback(null, retA.result || retB.result);
-    }
-  });
 };
 
 // Fetches a game invitation between 2 players.
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@
 
 const http = require('http');
 const util = require('util');
+const async = require('async');
 const lodash = require('lodash');
 const request = require('request');
 
@@ -17,6 +18,70 @@ const safeReq = (opts, callback) => {
 module.exports = {
   arrays: {
     equalWhenSorted: (a, b) => lodash.isEqual(a.sort(), b.sort())
+  },
+
+  // Tries calling functions one after the other.
+  // Whichever returns result, "wins", meaning others won't be run.
+  // If previous returns null, tries the other.
+  // If previous fails, all fail.
+  // If all return null, result is null.
+  firstResult (...args) {
+    const funcs = args.slice(0, -1);
+    const callback = args[args.length - 1];
+
+    let currentFunction = 0;
+    let currentResult = null;
+
+    const callFn = (cb) => {
+      const fn = funcs[currentFunction++];
+
+      fn((err, result) => {
+        if (err)
+          return cb(err);
+
+        currentResult = result;
+        cb();
+      });
+    };
+
+    async.whilst(
+      () => !currentResult && (currentFunction < funcs.length),
+      callFn,
+      (err) => callback(err, currentResult)
+    );
+  },
+
+  // Tries calling fnA and fnB.
+  // If both succeed, checks that JSON documents are the same, cb(err) otherwise.
+  // If one succeeds, returns that.
+  // If none succeed, returns error of fnA.
+  tryBoth (fnA, fnB, callback) {
+    const try_ = (fn) => (cb) => {
+      fn((err, result) => cb(null, {
+        err: err || null,
+        result: err ? null : result
+      }));
+    };
+
+    const tries = [fnA, fnB].map(try_);
+
+    async.parallel(tries, (err, [retA, retB]) => {
+      if (err)
+        return callback(err);
+
+      if (retA.err && retB.err) {
+        callback(retA.err);
+      }
+      else if (retA.result && retB.result) {
+        const same = lodash.isEqual(retA.result, retB.result);
+        return same
+          ? callback(null, retA.result)
+          : callback(new Error('tryBoth() got conflicting result'));
+      }
+      else {
+        callback(null, retA.result || retB.result);
+      }
+    });
   },
 
   getJson (uri, callback) {
